5c9bea50f08fceb84f3370e950de5bec27785daf
S. Gospodinov <svilen_dan@abv.bg>
2022-01-02 22:18:32 +0200

    tomato

--- app.js
+++ app.js
@@ -54,7 +54,22 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+function showNotification() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
 
+showNotification();
 
 
 
