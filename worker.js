59466a0615179d189dca631610147ad2bb2dba36
S. Gospodinov <svilen_dan@abv.bg>
2022-01-03 01:01:21 +0200

    tomato

--- worker.js
+++ worker.js
@@ -3,7 +3,11 @@ function workerFN() {
       switch(e.data.name) {
         case "notification" : 
           console.log("Notification:");
-          var notification = new Notification("Hi there!");
+          var options = {
+            body: 'Do you like my body?',
+            vibrate: [200, 100, 200]
+          }
+          var notification = new Notification("Hi there!", options);
         break;
         default:
           console.error("Unknown message:", e.data.name);
