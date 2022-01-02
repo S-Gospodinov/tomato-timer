4c23471bac475c757f1f420e94b9db91ff394e69
S. Gospodinov <svilen_dan@abv.bg>
2022-01-02 17:52:13 +0200

    tomato

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ let alertSound = new Audio('./alerts/gong.mp3');
 let alerNane = 'gong';
 
 
+
+
+const img = "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg";
+const text = "Take a look at this brand new t-shirt!";
+const title = "New Product Available";
+const options = {
+    body: text,
+    icon: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
+    vibrate: [200, 100, 200],
+    tag: "new-product",
+    image: img,
+    badge: "https://spyna.it/icons/android-icon-192x192.png",
+    actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+ };
+
+navigator.serviceWorker.ready.then(function(serviceWorker) {
+  serviceWorker.showNotification(title, options);
+});
+
+
+
+
 // Disable buttons onload, since Firefox sometimes chaches previous button state
 // see -> https://bugzilla.mozilla.org/show_bug.cgi?id=654072 
 window.addEventListener('load', () => {
